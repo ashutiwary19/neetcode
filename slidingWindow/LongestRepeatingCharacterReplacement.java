8d34e3f1953115681fe25ea1cf1b80e209f11296
ashutiwary19 <ashu.tiwary19@gmail.com>
2024-09-20 04:33:55 +0530

    Solved slidinding with k window size and slide

--- slidingWindow/LongestRepeatingCharacterReplacement.java
+++ slidingWindow/LongestRepeatingCharacterReplacement.java
@@ -1,7 +1,44 @@
 package slidingWindow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
-        return 0;
+        int majority = 0;
+        int solution = 0;
+        int[] charMap = new int[128];
+        for (int i = 0, l = 0; i < s.length(); i++) {
+            if (majority < ++charMap[s.charAt(i)]) {
+                majority = charMap[s.charAt(i)];
+            }
+            while (i - l + 1 - majority > k && i >= l) {
+                charMap[s.charAt(l++)]--;
+                for (int j = 0; j < 128; j++) {
+                    majority = Math.max(majority, charMap[j]);
+                }
+            }
+            solution = Math.max(solution, i - l + 1);
+
+        }
+
+        return solution;
+    }
+
+    public int characterReplacement2(String s, int k) {
+        int[] arr = new int[26];
+        int ans = 0;
+        int max = 0;
+        int i = 0;
+        for (int j = 0; j < s.length(); j++) {
+            arr[s.charAt(j) - 'A']++;
+            max = Math.max(max, arr[s.charAt(j) - 'A']);
+            if (j - i + 1 - max > k) {
+                arr[s.charAt(i) - 'A']--;
+                i++;
+            }
+            ans = Math.max(ans, j - i + 1);
+        }
+        return ans;
     }
 }
